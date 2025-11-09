e54c767adcedb2f8ea3d0bbeea5416b31e0535b1
Owen Schumacher <owenschu7@gmail.com>
2025-11-08 20:57:04 -0800

    added baccarat_hand.h and .cpp files

---
+++ baccarat_hand.cpp
@@ -0,0 +1,40 @@
+#include "Baccarat_hand.h"
+
+
+Baccarat_hand::Baccarat_hand()
+{
+
+}
+Baccarat_hand::Baccarat_hand(Card first_card)
+{
+
+}
+Baccarat_hand::Baccarat_hand(Card first_card, Card second_card)
+{
+
+}
+~Baccarat_hand::Baccarat_hand()
+{
+
+}
+void Baccarat_hand::add_third_card(Card third_card)
+{
+
+}
+Card Baccarat_hand::get_first_card()
+{
+
+}
+Card Baccarat_hand::get_second_card()
+{
+
+}
+Card Baccarat_hand::get_third_card()
+{
+
+}
+
+int Baccarat_hand::get_total()
+{
+
+}
